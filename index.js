0258c65cca7004ea7bacc1403b162785257233f9
CorellCB <corellcb@gmail.com>
2019-11-11 17:09:02 -0500

    completed challenge 2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function makePersonObject(id, name, email) {
       name: name,
       email: email
     };
-};
+}
 
 /**
  * ### Challenge `getName`
@@ -60,8 +60,8 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(obj) {
+  return 'Hello, my name is ' + obj.name;
 }
 
 /**
